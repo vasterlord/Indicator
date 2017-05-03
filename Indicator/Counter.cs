a8ab38c03e71b84c82f2c5ff317edca0c9a03591
vasterlord <vasterlord619@gmail.com>
2017-05-03 11:15:00 +0300

    Worked edition!

--- Indicator/Counter.cs
+++ Indicator/Counter.cs
@@ -4,73 +4,143 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Indicator
 {
-    public class Counter
+    public abstract class Counter
     {
-        private double maxvalue { get; set; }
-        private double minvalue { get; set; }
-        public double maxValue { get { return maxvalue; } set { maxvalue = value; } }
-        public double minValue { get { return minvalue; } set { minvalue = value; } }
-        private double value { get; set; }
-        public  double Value
+        public Counter()
         {
-            get { return value; }
-            set
-            {
-                if ((value > maxValue) || (value < minValue))
-                {
-                    this.value = 0;
-                }
-                else { this.value = value; }
-
-            }
-        }
-        public  Counter()
-        {
-            this.maxvalue = 5000;
-            this.minvalue = 0;
-            this.value = 0;
+            this._minValue = 0;
+            this._maxValue = 5000;
+            this._value = 0;
         }
 
-        public  Counter(int max, int min)
+        public Counter(int max, int min, double value)
         {
-            this.maxValue = max;
-            this.minValue = min;
-        }
-        
-    }
-    public class Electricity_meter : Counter,IEnumerable
-    {
-        public Electricity_meter(int max , int accuracy,double value)
+            this._minValue = min;
+            this._maxValue = max;
+            this._value = value;
+        } 
+
+        private double _maxValue;
+        private double _minValue;
+        private double _value;
+        public double MaxValue
         {
-            this.maxValue = max;
-            this.accuracy = accuracy;
-            this.Value = value;
-            this.minValue = 0;
-        }
-        //Розрядність 
-        private  int Bit;
-        public  int bit
-        {
-            get { return Bit; }
+            get
+            {
+                return _maxValue;
+            }
             set
             {
-                if ((value == 5) || (value == 7) || (value == 8))
+                if (value <= 0)
                 {
-                    Bit = value;
+                    _maxValue = 5000;
+                    throw new FormatException("Incorrect maximum value of electricity meter! This value setted by default: 5000");
                 }
-                else Bit = 5;
+                else
+                {
+                    _maxValue = value;
+                }
             }
         }
-        private  int Accuracy;
-        public  int accuracy
+        public double MinValue
         {
-            get { return Accuracy; }
+            get
+            {
+                return _minValue;
+            }
             set
             {
-                Accuracy = value;
+                if (value < 0)
+                {
+                    _maxValue = 0;
+                    throw new FormatException("Incorrect mainimumm value of electricity meter! This value setted by default: 0");
+                }
+                else
+                {
+                    _minValue = value;
+                }
+            }
+        }
+        public double Value
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                if ((value >= MaxValue) || (value < MinValue))
+                {
+                    this._value = 0;
+                }
+                else if (value < 0)
+                {
+                    throw new FormatException("Incorrect value of electricity meter! This value setted by default: 0");
+                }
+                else
+                {
+                    this._value = value;
+                }
+            }
+        }
+        public abstract List<int> Show();
+    } 
+
+    public class Electricity_meter : Counter, IEnumerable
+    {
+        public Electricity_meter() : base()
+        {
+            this.MinValue = 0;
+            this.MaxValue = 5000;
+            this.Value = 0;
+        }
+        public Electricity_meter(int max, int min ,double value, int accuracy, int bit ):base(max,min,value)
+        {
+            this._accuracy = accuracy;
+            this._bit = bit;
+        }
+        private  int _bit;
+        private int _accuracy;
+        public  int Bit
+        {
+            get
+            {
+                return _bit;
+            }
+            set
+            {
+                if (value > 8)
+                {
+                    _bit = 8;
+                    throw new FormatException("Incorrect enterd bit value!This value setted by default: 8 bit");
+                }
+                else
+                {
+                   _bit = value; 
+                }
+            }
+        }
+        public  int Accuracy
+        {
+            get
+            {
+                return _accuracy;
+            }
+            set
+            {
+                if (_accuracy < 0 || _accuracy >= _bit)
+                {
+                    _accuracy = 1;
+                    throw new FormatException("Incorrect enterd accuracy value!This value setted by default: 1");
+                }
+                else
+                {
+                    _accuracy = value;
+                }
             }
         }
         public  IEnumerator GetEnumerator()
@@ -78,56 +148,95 @@ namespace Indicator
             yield return this.Value;
             
         }
-        public List<int> Show()
-        {
-            List<int> result=new List<int>();
-            string Numbers = Value.ToString();
-            
-        var intList = Numbers.Select(digit => int.Parse(digit.ToString()));
 
-        foreach (char i in intList)
+        public override List<int> Show()
+        {
+            List<int> result = new List<int>();
+            string Numbers = Value.ToString();
+
+            var intList = Numbers.Select(digit => int.Parse(digit.ToString()));
+
+            foreach (char i in intList)
             {
-                result.Add(i);   
+                result.Add(i);
             }
             return result;
         }
-        public static Electricity_meter operator ++(Electricity_meter a)
+        public static Electricity_meter operator ++(Electricity_meter counter)
         {
-            a.Value++;
-            return a;
+            counter.Value++;
+            return counter;
         }
-        public static Electricity_meter operator --(Electricity_meter a)
+        public static Electricity_meter operator --(Electricity_meter counter)
         {
-            a.Value--;
-            return a;
+            counter.Value--;
+            return counter;
         }
-        
     }
+
     public class Calculated_book
-    {
-        private double worth;
-        public double Worth { get { return worth; } set { worth = Math.Round(value, 2); } }
-        public DateTime Time { get; set; }
-        public double price { get; set; }
+    { 
         public Calculated_book()
         {
+            this._price = 10;
+            this.Date = "January";
         }
-        public Calculated_book(double price)
+        public Calculated_book(double price, string date)
         {
-            this.price = price;
+            this._price = price;
+            Date = date;
         }
+        public double _price { get; set; }
+        private string _date; 
+        public double Price
+        {
+            get
+            {
+                return _price;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    _price = 10;
+                    throw new FormatException("Incorrect enterd price value!This value setted by default: 10");
+                }
+                else
+                {
+                    _price = value;
+                }
+            }
+        }
+        public string Date
+        {
+            get
+            {
+                return _date;
+            }
+            set
+            {
+                if (_date == null || _date == "" || _date == " " || _date == string.Empty)
+                {
+                    _date = "January";
+                }
+                else
+                {
+                    _date = value;
+                }
+            }
+        }
+        public double CommonSum { get; set; }
         public List<Electricity_meter> Book = new List<Electricity_meter>();
-        public double Calculate_price(double worth)
-       {
-           Worth = worth;
-           double WholeElectricity=0;
+        public double Calculate_price()
+        {
+           double WholeElectricity = 0;
            foreach (var i in Book)
            {
-               WholeElectricity +=(double) i.Value/Math.Pow(10,i.accuracy); 
+               WholeElectricity +=(double) i.Value/Math.Pow(10,i.Accuracy); 
            }
-           price = WholeElectricity * Worth;
-           return price;
+           CommonSum = WholeElectricity * _price;
+           return CommonSum;
        }
-        public double[] Total_prices = new double[12] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        public double[] Total_prices = new double[12] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
     }
 }
--- Indicator/MainWindow.xaml.cs
+++ Indicator/MainWindow.xaml.cs
@@ -50,11 +50,11 @@ namespace Indicator
             showL.Reverse();
             showG.Reverse();
             var bc = new BrushConverter();
-            for (int i = 0; i < CBook.Book[CBook.Book.Count - 1].accuracy; i++)
+            for (int i = 0; i < CBook.Book[CBook.Book.Count - 1].Accuracy; i++)
             {
                 showG[i].Background = (Brush)bc.ConvertFrom("#FFAC0000");
             }
-            for (int i = CBook.Book[CBook.Book.Count - 1].accuracy; i < showG.Count; i++)
+            for (int i = CBook.Book[CBook.Book.Count - 1].Accuracy; i < showG.Count; i++)
             {
                 showG[i].Background = (Brush)bc.ConvertFrom("#FF746060");
             }
@@ -64,12 +64,12 @@ namespace Indicator
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
             double CounterValue =  Convert.ToDouble(tb_value_of_new_Counter.Text);
-          
-            CBook.Book.Add(new Electricity_meter(Convert.ToInt32(l_max.Content), Convert.ToInt32(l_accuracy.Text), CounterValue));
-
+            CBook.Book.Add(new Electricity_meter(Convert.ToInt32(l_max.Content), 0,  CounterValue, Convert.ToInt32(l_accuracy.Text), Convert.ToInt32(l_bit1.Text)));
             ShowIntoEkran(CBook);
-
-            tb_all_price_of_electrisity.Text = Convert.ToString(CBook.Calculate_price(Convert.ToDouble(tb_price.Text)));
+            Designcounter();
+            CBook.Price = Convert.ToDouble(tb_price.Text);
+            CBook.Date = Calendar.Text;
+            tb_all_price_of_electrisity.Text = Convert.ToString(CBook.Calculate_price());
         }
 
         private void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
@@ -82,70 +82,109 @@ namespace Indicator
         {
             CBook.Book[CBook.Book.Count - 1]--;
             ShowIntoEkran(CBook);
-            tb_all_price_of_electrisity.Text = Convert.ToString(CBook.Calculate_price(Convert.ToDouble(tb_price.Text)));
+            CBook.Price = Convert.ToDouble(tb_price.Text);
+            CBook.Date = Calendar.Text;
+            tb_all_price_of_electrisity.Text = Convert.ToString(CBook.Calculate_price());
         }
 
         private void b_down_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             CBook.Book[CBook.Book.Count - 1]++;
             ShowIntoEkran(CBook);
-            tb_all_price_of_electrisity.Text = Convert.ToString(CBook.Calculate_price(Convert.ToDouble(tb_price.Text)));
+            CBook.Price = Convert.ToDouble(tb_price.Text);
+            CBook.Date = Calendar.Text;
+            tb_all_price_of_electrisity.Text = Convert.ToString(CBook.Calculate_price());
         }
 
         private void tb_price_TextChanged(object sender, TextChangedEventArgs e)
         {
-            tb_all_price_of_electrisity.Text = Convert.ToString(CBook.Calculate_price(Convert.ToDouble(tb_price.Text)));
+            CBook.Price = Convert.ToDouble(tb_price.Text);
+            tb_all_price_of_electrisity.Text = Convert.ToString(CBook.Calculate_price()); 
         }
 
-        private void Calendar_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            tb_all_price_of_electrisity.Text = CBook.Total_prices[Calendar.SelectedIndex].ToString();
-        }
-
-        private void btn_Safe_total_price_Click(object sender, RoutedEventArgs e)
-        {
-            CBook.Total_prices[Calendar.SelectedIndex] = Math.Round(( Convert.ToDouble(tb_all_price_of_electrisity.Text)),2);
-        }
-
-        private void l_bit1_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-                int bit = Convert.ToInt32(l_bit1.Text);
-            
-                int MakeInvalid = showL.Count - bit;
-                showL.Reverse();
-                for (int i = bit; i < showL.Count; i++)
-                {
-                    showL[i].Visibility = Visibility.Collapsed;
-                }
-                for (int i = 0; i < bit; i++)
-                {
-                    showL[i].Visibility = Visibility.Visible;
-                }
-                showL.Reverse();
-                slider.Maximum = Math.Pow(10, bit);
+            CBook.Book[CBook.Book.Count - 1].Bit = Convert.ToInt32(l_bit1.Text);
+            Designcounter();
+            slider.Maximum = Math.Pow(10, CBook.Book[CBook.Book.Count - 1].Bit);
+            tb_all_price_of_electrisity.Text = Convert.ToString(CBook.Calculate_price());
+        }
+
+        private void Designcounter()
+        {
+            int MakeInvalid = showL.Count - CBook.Book[CBook.Book.Count - 1].Bit;
+            showL.Reverse();
+            for (int i = CBook.Book[CBook.Book.Count - 1].Bit; i < showL.Count; i++)
+            {
+                showL[i].Visibility = Visibility.Collapsed;
+            }
+            for (int i = 0; i < CBook.Book[CBook.Book.Count - 1].Bit; i++)
+            {
+                showL[i].Visibility = Visibility.Visible;
+            }
+            showL.Reverse();
         }
 
         private void Button_Click_3(object sender, RoutedEventArgs e)
         {
-            int accuracy = Convert.ToInt32(l_accuracy.Text); 
+            CBook.Book[CBook.Book.Count - 1].Accuracy = Convert.ToInt32(l_accuracy.Text); 
             showG.Reverse();
             var bc = new BrushConverter();
-
-            for (int i = 0; i < accuracy; i++)
+            for (int i = 0; i < CBook.Book[CBook.Book.Count - 1].Accuracy; i++)
             {
-                //
                 showG[i].Background = (Brush)bc.ConvertFrom("#FFAC0000");
             }
-            for (int i = accuracy; i < showG.Count; i++)
+            for (int i = CBook.Book[CBook.Book.Count - 1].Accuracy; i < showG.Count; i++)
             {
                 showG[i].Background = (Brush)bc.ConvertFrom("#FF746060");
             }
             showG.Reverse();
+            tb_all_price_of_electrisity.Text = Convert.ToString(CBook.Calculate_price());
+            Designcounter();
+        }
+
+        private void l_bit1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                if (l_bit1.Text == string.Empty || l_bit1.Text == "0" || l_bit1.Text == "" || l_bit1.Text == " ")
+                {
+                    slider.Maximum = Math.Pow(10, 8);
+                }
+                else
+                {
+                    slider.Maximum = Math.Pow(10, Convert.ToDouble(l_bit1.Text));
+                }
+            }
+            catch (FormatException)
+            {
+                slider.Maximum = Math.Pow(10, 8);
+            } 
+        }
+        private void Calendar_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (CBook.Total_prices[Calendar.SelectedIndex] == 0)
+            {
+                tb_all_price_of_electrisity.Text = "0";
+            }
+            else
+            {
+                tb_all_price_of_electrisity.Text = CBook.Total_prices[Calendar.SelectedIndex].ToString();
+            }
+        }
+
+        private void btn_Safe_total_price_Click(object sender, RoutedEventArgs e)
+        {
+            CBook.Total_prices[Calendar.SelectedIndex] = Math.Round((Convert.ToDouble(tb_all_price_of_electrisity.Text)), 2);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            CBook.Book[CBook.Book.Count - 1].Value = Convert.ToInt32(tb_value_of_new_Counter.Text);
+            ShowIntoEkran(CBook);
+            tb_all_price_of_electrisity.Text = Convert.ToString(CBook.Calculate_price());
+            Designcounter();
         }
     }
 }
